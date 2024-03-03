2c878b21b777d6c378eb69e86fdfff26709a0f80
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-03 18:41:56 +0900

    test: decode_hex_to_bytes

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,25 @@ pub fn convert_ascii_integers_to_chars(ascii_integers: Vec<u8>) -> Vec<char> {
 
 pub fn decode_hex_to_bytes(hex_string: &str) -> Result<Vec<u8>, &'static str> {
     let result: Vec<u8> = vec![];
-    return Ok(result);
+    let padded_hex_string = if hex_string.len() % 2 == 0 {
+        hex_string.to_owned()
+    } else {
+        format!("0{}", hex_string)
+    };
+
+    let mut bytes = Vec::with_capacity(padded_hex_string.len() / 2);
+
+    for i in (0..padded_hex_string.len()).step_by(2) {
+        let hex_pair = &padded_hex_string[i..i + 2];
+
+        if let Ok(byte) = u8::from_str_radix(hex_pair, 16) {
+            bytes.push(byte);
+        } else {
+            return Err("Invalid hexadecimal character");
+        }
+    }
+
+    Ok(bytes)
 }
 
 #[cfg(test)]
@@ -29,5 +47,17 @@ mod tests {
     #[test]
     fn test_decode_hex_to_bytes() {
         assert_eq!(Ok(vec![]), decode_hex_to_bytes(""));
+        assert_eq!(Ok(vec![0]), decode_hex_to_bytes("0"));
+        assert_eq!(Ok(vec![9]), decode_hex_to_bytes("9"));
+        assert_eq!(Ok(vec![10]), decode_hex_to_bytes("A"));
+        assert_eq!(Ok(vec![15]), decode_hex_to_bytes("F"));
+
+        let bytes = decode_hex_to_bytes("63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d").unwrap();
+        let chars = convert_ascii_integers_to_chars(bytes);
+        let encoded_msg: String = chars.into_iter().collect();
+        assert_eq!(
+            encoded_msg,
+            "crypto{You_will_be_working_with_hex_strings_a_lot}",
+        );
     }
 }
