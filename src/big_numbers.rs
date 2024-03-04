94782048006ade862274f053efc567c613adb386
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-04 18:46:31 +0900

    chore: add from<Base10> setup for Base16

--- src/big_numbers.rs
+++ src/big_numbers.rs
@@ -3,7 +3,21 @@ mod big_numbers {
         value: String,
     }
 
+    impl Base10 {
+        pub fn new(value: String) -> Self {
+            Self { value }
+        }
+    }
+
     pub struct Base16 {
         value: String,
+    }
+
+    impl From<Base10> for Base16 {
+        fn from(decimal_number: Base10) -> Self {
+            Self {
+                value: "".to_string(),
+            }
+        }
     }
 }
