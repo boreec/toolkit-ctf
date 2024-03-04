4f293db2878fb097aad0a4753fc3530b76f2337b
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-04 18:54:21 +0900

    chore: impl PartialEq on big numbers

--- src/big_numbers.rs
+++ src/big_numbers.rs
@@ -1,4 +1,5 @@
 mod big_numbers {
+    #[derive(Debug)]
     pub struct Base10 {
         value: String,
     }
@@ -9,15 +10,47 @@ mod big_numbers {
         }
     }
 
+    impl PartialEq for Base10 {
+        fn eq(&self, other: &Self) -> bool {
+            self.value == other.value
+        }
+    }
+
+    #[derive(Debug)]
     pub struct Base16 {
         value: String,
+    }
+
+    impl Base16 {
+        pub fn new(value: String) -> Self {
+            Self { value }
+        }
+    }
+
+    impl PartialEq for Base16 {
+        fn eq(&self, other: &Self) -> bool {
+            self.value == other.value
+        }
     }
 
     impl From<Base10> for Base16 {
         fn from(decimal_number: Base10) -> Self {
             Self {
-                value: "".to_string(),
+                value: "0".to_string(),
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::big_numbers::*;
+
+    #[test]
+    fn test_base16_from_base10() {
+        assert_eq!(
+            Base16::from(Base10::new("0".to_string())),
+            Base16::new("0".to_string())
+        );
     }
 }
