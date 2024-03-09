11c163ae0757de6ddf2782f0016d22877998ce1a
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-09 19:21:01 +0900

    chore: remove default lib code

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,3 @@ pub mod big_numbers;
 pub mod generators;
 mod primality;
 mod utils;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
