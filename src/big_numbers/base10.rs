9c139fbeb6eb7ce562f21bdaab2a32215b335b61
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-10 18:14:40 +0900

    feat: implement Base10::try_from(string)

--- src/big_numbers/base10.rs
+++ src/big_numbers/base10.rs
@@ -47,29 +47,40 @@ impl PartialEq for Base10 {
     }
 }
 
-impl TryFrom<&str> for Base16 {
+impl TryFrom<&str> for Base10 {
     type Error = Box<dyn Error>;
 
     fn try_from(decimal_string: &str) -> Result<Self, Box<dyn Error>> {
         if decimal_string.is_empty() {
             return Ok(Self { bytes: Vec::new() });
         }
-        Base10::validate_hex_string(decimal_string)?;
-        let simplified_hex_string = Base10::simplify_hex_string(hex_string);
+        Base10::valide_decimal_string(decimal_string)?;
+        let simplified_decimal_string =
+            Base10::simplify_decimal_string(decimal_string);
 
-        let padded_hex_string = if simplified_hex_string.len() % 2 == 0 {
-            simplified_hex_string.to_owned()
-        } else {
-            format!("0{}", simplified_hex_string)
-        };
+        // rough estimate
+        let bytes_capacity = simplified_decimal_string.len() as f64 * 0.3;
+        let mut bytes = Vec::with_capacity(bytes_capacity as usize);
 
-        let mut bytes = Vec::with_capacity(padded_hex_string.len() / 2);
+        // concert decimal string to bytes string here
+        let mut current_byte = 0;
+        let mut current_digit_count = 0;
 
-        for i in (0..padded_hex_string.len()).step_by(2) {
-            let hex_pair = &padded_hex_string[i..i + 2];
+        for digit in simplified_decimal_string.chars() {
+            current_byte = current_byte * 10
+                + digit.to_digit(10).ok_or("Invalid digit")? as u8;
+            current_digit_count += 1;
 
-            let byte = u8::from_str_radix(hex_pair, 16)?;
-            bytes.push(byte);
+            if current_digit_count == 3 {
+                bytes.push(current_byte);
+                current_byte = 0;
+                current_digit_count = 0;
+            }
+        }
+
+        // Handle the remaining digits
+        if current_digit_count > 0 {
+            bytes.push(current_byte);
         }
 
         bytes.reverse();
