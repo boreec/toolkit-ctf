586d6ab7be826af781541dec253bdf218c66d7ee
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-11 19:16:56 +0900

    chore: make base64 valid chars global

--- src/big_numbers/base64.rs
+++ src/big_numbers/base64.rs
@@ -1,27 +1,37 @@
+use std::error::Error;
+
+const BASE_64_CHARS: &[char] = &[
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', '+', '/',
+];
+
 pub struct Base64 {
     bytes: Vec<u8>,
 }
 
 impl Base64 {
     pub fn validate_base64_string(base64_string: &str) -> Result<(), String> {
-        let valid_chars: Vec<char> =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-                .chars()
-                .collect();
-
         for c in base64_string.chars() {
-            if !valid_chars.contains(&c) {
+            if !BASE_64_CHARS.contains(&c) {
                 return Err(format!("{} is not a correct digit in base 64", c));
             }
         }
         Ok(())
     }
 }
-// impl TryFrom<&str> for Base64 {
-//     fn try_from(value: &str) -> Result<Self, Self::Error> {
-//         Ok(())
-//     }
-// }
+
+impl TryFrom<&str> for Base64 {
+    type Error = Box<dyn Error>;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Base64::validate_base64_string(value)?;
+
+        Ok(Self { bytes: vec![] })
+    }
+}
 
 #[cfg(test)]
 mod tests {
