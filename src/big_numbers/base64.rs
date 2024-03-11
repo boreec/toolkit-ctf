f65c61baa46e5e1e8aabf989f06473c161f8ae87
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-11 19:02:55 +0900

    test: base64::validate_base64_string()

--- src/big_numbers/base64.rs
+++ src/big_numbers/base64.rs
@@ -1,11 +1,19 @@
-use std::error::Error;
-
 pub struct Base64 {
     bytes: Vec<u8>,
 }
 
 impl Base64 {
     pub fn validate_base64_string(base64_string: &str) -> Result<(), String> {
+        let valid_chars: Vec<char> =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+                .chars()
+                .collect();
+
+        for c in base64_string.chars() {
+            if !valid_chars.contains(&c) {
+                return Err(format!("{} is not a correct digit in base 64", c));
+            }
+        }
         Ok(())
     }
 }
@@ -14,3 +22,18 @@ impl Base64 {
 //         Ok(())
 //     }
 // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_validate_base64_string() {
+        assert!(Base64::validate_base64_string("").is_ok());
+        assert!(Base64::validate_base64_string(
+            "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu"
+        )
+        .is_ok());
+        assert!(Base64::validate_base64_string("???????").is_err());
+    }
+}
