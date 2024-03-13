f67000b25e7341db89af0ab215562f3bdff6f634
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-13 18:49:22 +0900

    test: remove_leading_zeros

--- examples/base16.rs
+++ examples/base16.rs
@@ -1,6 +1,7 @@
 extern crate toolkit_ctf;
 
 use toolkit_ctf::big_numbers::base16::Base16;
+use toolkit_ctf::big_numbers::remove_leading_zeros;
 
 fn main() {
     let hex_string = "0000BADCAFE";
@@ -11,6 +12,6 @@ fn main() {
         Base16::validate_hex_string(hex_string).is_ok(),
     );
 
-    let simplified_hex_string = Base16::simplify_hex_string(hex_string);
+    let simplified_hex_string = remove_leading_zeros(hex_string);
     println!("simplified hex_string: {}", simplified_hex_string);
 }
--- src/big_numbers/mod.rs
+++ src/big_numbers/mod.rs
@@ -18,3 +18,17 @@ pub fn remove_leading_zeros(number_string: &str) -> String {
     }
     return number_string[leading_zeros..].to_string();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_leading_zeros() {
+        assert_eq!("0", remove_leading_zeros("0"));
+        assert_eq!("0", remove_leading_zeros("00000"));
+        assert_eq!("10000", remove_leading_zeros("10000"));
+        assert_eq!("10001", remove_leading_zeros("0010001"));
+        assert_eq!("BADCAFE", remove_leading_zeros("00BADCAFE"));
+    }
+}
