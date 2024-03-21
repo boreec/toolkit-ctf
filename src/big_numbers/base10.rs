7679e584a9ef41095b6475bbb2a7cebcc1881964
Cyprien Boree <cyprien.boree@tuta.io>
2024-03-21 18:59:50 +0900

    test: Base10::try_from_string()

--- src/big_numbers/base10.rs
+++ src/big_numbers/base10.rs
@@ -42,34 +42,30 @@ impl TryFrom<&str> for Base10 {
             return Ok(Self { bytes: Vec::new() });
         }
         Base10::validate_decimal_string(decimal_string)?;
-        let simplified_decimal_string = remove_leading_zeros(decimal_string);
+        let mut simplified_string = remove_leading_zeros(decimal_string);
 
         // rough estimate
-        let bytes_capacity = simplified_decimal_string.len() as f64 * 0.3;
+        let bytes_capacity = simplified_string.len() as f64 * 0.3;
         let mut bytes = Vec::with_capacity(bytes_capacity as usize);
 
-        // convert decimal string to bytes vector
-        let mut current_byte = 0;
-        let mut current_digit_count = 0;
-
-        for digit in simplified_decimal_string.chars() {
-            current_byte = current_byte * 10
-                + digit.to_digit(10).ok_or("Invalid digit")? as u8;
-            current_digit_count += 1;
-
-            if current_digit_count == 3 {
-                bytes.push(current_byte);
-                current_byte = 0;
-                current_digit_count = 0;
+        while !simplified_string.is_empty() {
+            let slice_size = 3.min(simplified_string.len());
+            let slice_chars =
+                &simplified_string[simplified_string.len() - slice_size..];
+            let slice_value: u16 = slice_chars.parse()?;
+            if slice_value > 255 {
+                bytes.push(255u8);
+                let remainder = slice_value - 255;
+                simplified_string
+                    .truncate(simplified_string.len() - slice_size);
+                simplified_string.push_str(&remainder.to_string());
+            } else {
+                bytes.push(slice_value as u8);
+                simplified_string
+                    .truncate(simplified_string.len() - slice_size);
             }
         }
 
-        // Handle the remaining digits
-        if current_digit_count > 0 {
-            bytes.push(current_byte);
-        }
-
-        bytes.reverse();
         Ok(Self { bytes })
     }
 }
@@ -87,5 +83,14 @@ mod tests {
         assert!(Base10::validate_decimal_string("0x0000").is_err());
         assert!(Base10::validate_decimal_string("dsafhodhfo").is_err());
         assert!(Base10::validate_decimal_string("BADCAFE").is_err());
+    }
+
+    #[test]
+    fn test_try_from_string() {
+        assert!(Base10::try_from("0").is_ok());
+        assert!(Base10::try_from("adjsakjdlas").is_err());
+        assert_eq!(vec![0u8], Base10::try_from("0").unwrap().bytes);
+        assert_eq!(vec![255u8], Base10::try_from("255").unwrap().bytes);
+        assert_eq!(vec![255u8, 1u8], Base10::try_from("256").unwrap().bytes);
     }
 }
